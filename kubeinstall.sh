6f631031ef7a07bbfbe4577629bc1c4a1d912c52
Ovidiu Borlean <ovidiu.borlean@gmail.com>
2024-10-21 09:37:28 +0300

    Update kubeinstall.sh

--- kubeinstall.sh
+++ kubeinstall.sh
@@ -4,6 +4,39 @@ get_latest_release() {
     | sed -E 's/.*"([^"]+)".*/\1/'
 }
 
+host_config () {
+  # See if swap is enabled
+swapon --show
+
+# Turn off swap
+sudo swapoff -a
+
+# Disable swap completely
+sudo sed -i -e '/swap/d' /etc/fstab
+
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe -a overlay br_netfilter
+
+# sysctl params required by setup, params persist across reboots
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+# Apply sysctl params without reboot
+sudo sysctl --system
+
+sudo ufw allow 6443/tcp
+
+#sudo systemctl stop apparmor
+#sudo systemctl disable apparmor
+}
+
 install_cni () {
 
 CNI_VERS=$(get_latest_release containernetworking plugins) # v1.5.0
